0450137cf003639d31ead5717e58e00918f88bb9
Edthechamp <e.ernests.mathers@gmail.com>
2022-11-25 21:24:45 +0200

    base window

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Yea")
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+kivy.require('1.9.0')
+
+class yeah(App):
+    def build(self):
+        return Label(text="workin type beat")
+
+yeah = yeah()
+yeah.run()
