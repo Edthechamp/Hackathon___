4ab22ca8d26222b9a902b0313228353f647f5c8e
Edthechamp <e.ernests.mathers@gmail.com>
2022-11-26 19:51:11 +0200

    base window

--- App.py
+++ App.py
@@ -4,19 +4,30 @@ from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
-
+tasks = []
+with open('data.txt', 'r+') as f:
+    listpos = f.readline()
 class SayHello(App):
+    def callback(self, instance):
+        print(self.user.text)
+        with open('data.txt', 'a') as f:
+            f.append = self.user.text
+
     def build(self):
         self.window = GridLayout()
         self.window.cols = 1
-
-        self.window.add_widget(Label(text="paruski"))
+        self.window.size_hint = (0.6,0.7)
+        self.window.pos_hint = {"center_x":0.5, "center_y":0.5}
+        self.label = Label(text="ToDo List:")
+        self.window.add_widget(self.label)
         #add widgets to window
 
         self.user = TextInput()
         self.window.add_widget(self.user)
 
-        self.button = Button(text="Hello")
+        self.button = Button(text="Add task:")
+        self.button.pos
+        self.button.bind(on_press=self.callback)
         self.window.add_widget(self.button)
 
 
