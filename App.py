c38d923f3115267505f03af7d6601e994d4c00f8
Edthechamp <e.ernests.mathers@gmail.com>
2022-11-27 12:23:23 +0200

    nu vnk ngl kinda aids

--- App.py
+++ App.py
@@ -1,32 +1,42 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.lang import Builder
 from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
-class SayHello(App):
+Builder.load_file('build.kv')
+class ToDoApp(App):
     def callback(self, instance):
-        print(self.user.text)
+
+
+        with open('data.txt', 'a') as f:
+            f.write(self.user.text + '\n')
+            f.close()
+        self.user.text = ''
+
+
 
     def build(self):
-        self.window = GridLayout()
-        self.window.cols = 1
-        self.window.size_hint = (0.6,0.7)
-        self.window.pos_hint = {"center_x":0.5, "center_y":0.5}
-        self.label = Label(text="ToDo List:")
-        self.window.add_widget(self.label)
-        #add widgets to window
+        # self.window = GridLayout()
+        # self.window.cols = 1
+        # self.window.size_hint = (0.6,0.7)
+        # self.window.pos_hint = {"center_x":0.5, "center_y":0.5}
+        # self.label = Label(text="ToDo List:")
+        # self.window.add_widget(self.label)
+        # #add widgets to window
+        #
+        # self.user = TextInput()
+        # self.window.add_widget(self.user)
+        #
+        # self.button = Button(text="Add task:")
+        # self.button.pos
+        # self.button.bind(on_press=self.callback)
+        # self.window.add_widget(self.button)
+        #
 
-        self.user = TextInput()
-        self.window.add_widget(self.user)
-
-        self.button = Button(text="Add task:")
-        self.button.pos
-        self.button.bind(on_press=self.callback)
-        self.window.add_widget(self.button)
-
-
-        return self.window
+        return FloatLayout()
 
 if __name__ == "__main__":
-    SayHello().run()
+    ToDoApp().run()
